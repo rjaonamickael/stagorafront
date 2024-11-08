45959427a61022664f4ee8fbf9bfdb337c1168b6
ksimali <cassim.ali@outlook.fr>
2024-11-08 00:06:17 -0500

    Add EtudiantInscriptionComponent to app.module & Create a route in app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './pages/admin/home/dashboard/dashboard.component';
 import { AdminHomeComponent } from './pages/admin/home/container/admin-home.component';
 import { EmployeurInscriptionComponent} from './pages/employeur/inscription-employeur/employeur-inscription.component';
+import { EtudiantInscriptionComponent } from './pages/etudiant/etudiant-inscription/etudiant-inscription.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },  
   { path: 'dashboard', component: DashboardComponent },       // Route vers le Dashboard (Accueil)
   { path: 'admin-home', component: AdminHomeComponent },      // Route vers la gestion des établissements
   { path: 'inscription-employeur', component: EmployeurInscriptionComponent }, // Route vers l'inscription des employeurs
-  { path: '**', redirectTo: '/dashboard' }                    // Redirection des routes inconnues vers le Dashboard
+  { path: 'inscription-etudiant', component: EtudiantInscriptionComponent }, // Route vers l'inscription des etudiants
+  { path: '**', redirectTo: '/dashboard' }  // Redirection des routes inconnues vers le Dashboard
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,19 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { EmployeurInscriptionComponent } from './pages/employeur/inscription-employeur/employeur-inscription.component';
+import { EtudiantInscriptionComponent } from './pages/etudiant/etudiant-inscription/etudiant-inscription.component';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AdminModule } from './pages/admin/admin.module';
 // src/models/employeur.model.ts
 import { Site } from '../models/site.model'; // Ajustez le chemin selon l’emplacement du fichier `site.model.ts`
 
+
 @NgModule({
   declarations: [
     AppComponent,
     EmployeurInscriptionComponent,
+    EtudiantInscriptionComponent,
   ],
   imports: [
     BrowserModule,
