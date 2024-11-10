2c6c77b402bf9adc72a8108be39a07163d850920
ksimali <cassim.ali@outlook.fr>
2024-11-10 11:16:58 -0500

    Setup Etudiant service : - Add inscrireEtudiant() method to register students in database via etudiantInscriptionForm

--- src/services/etudiant.service.ts
+++ src/services/etudiant.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EtudiantService {
+  private baseUrl = 'http://localhost:8082/user/inscription';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  inscrireEtudiant(data: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}/etudiant`, data);
+  }
 }
